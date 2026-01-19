7156419b624d042e121797286040d0134998ec0d
Administrator <anjebepu@gmail.com>
2026-01-18 21:56:19 -0300

    style: Formateo del archivo de las excepciones custom

--- src/core/exceptions_custom.py
+++ src/core/exceptions_custom.py
@@ -1,6 +1,6 @@
 class GatoError(Exception):
     """Excepción base para todos los errores del juego."""
-    
+
     def __init__(self, message: str):
         super().__init__("Error relacionado con el juego.\n" + message)
 
@@ -9,14 +9,18 @@ class JuegoTerminadoError(GatoError):
     """Se intenta jugar cuando el juego ya terminó."""
 
     def __init__(self):
-        super().__init__("El juego ya ha terminado. No se pueden hacer más movimientos.")
+        super().__init__(
+            "El juego ya ha terminado. No se pueden hacer más movimientos."
+        )
 
 
 class SubGatoTerminadoError(GatoError):
     """Se intenta jugar en un sub-gato que ya terminó."""
 
     def __init__(self):
-        super().__init__("El sub-gato ya ha terminado. No se pueden hacer más movimientos en él.")
+        super().__init__(
+            "El sub-gato ya ha terminado. No se pueden hacer más movimientos en él."
+        )
 
 
 class MovimientoInvalidoError(GatoError):
@@ -30,7 +34,9 @@ class FueraDeRangoError(MovimientoInvalidoError):
     """La fila o columna está fuera del tablero."""
 
     def __init__(self):
-        super().__init__("La fila o columna especificada está fuera del rango permitido del tablero.")
+        super().__init__(
+            "La fila o columna especificada está fuera del rango permitido del tablero."
+        )
 
 
 class CasillaOcupadaError(MovimientoInvalidoError):
