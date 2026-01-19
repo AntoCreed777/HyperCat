a9ffe856ecadfdfcb91e67bdd1ab4a6c0efd10a4
Administrator <anjebepu@gmail.com>
2026-01-18 21:55:53 -0300

    feat: Menu de seleccion de gatos implementados

--- src/main.py
+++ src/main.py
@@ -2,7 +2,24 @@ from ui import *
 
 
 def main():
-    VentanaHyperCat().run()
+    print(
+        "Que deseas jugar?",
+        "\n1. HyperCat",
+        "\n2. TicTacToe (Gato clasico)",
+        "\n\n"
+    )
+
+    while True:
+        eleccion = input("Ingresa el numero del juego: ")
+        match eleccion:
+            case "1":
+                VentanaHyperCat().run()
+                break
+            case "2":
+                VentanaGato().run()
+                break
+            case _:
+                print("Opcion no valida\n\n")
 
 
 if __name__ == "__main__":
